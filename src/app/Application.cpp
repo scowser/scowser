5c5cd19e029604efa5f8ba689d0d838d10db08de
Julio Jimenez <julio@julioj.com>
2026-04-03 15:10:39 -0400

    chore(release): v0.0.7
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- src/app/Application.cpp
+++ src/app/Application.cpp
@@ -17,7 +17,7 @@ Application::Application(int &argc, char **argv)
     : QApplication(argc, argv)
 {
     setApplicationName("scowser");
-    setApplicationVersion("0.0.5");
+    setApplicationVersion("0.0.7");
     setOrganizationName("scowser");
     setWindowIcon(QIcon(":/icons/scowser.png"));
 
