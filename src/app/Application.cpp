62629fe110569285c2d33aed7ba0091b77fa3915
Julio Jimenez <julio@julioj.com>
2026-04-03 14:25:24 -0400

    fix(release): JIT Entitlements (#18)
    Signed-off-by: Julio Jimenez <julio@julioj.com>

--- src/app/Application.cpp
+++ src/app/Application.cpp
@@ -17,7 +17,7 @@ Application::Application(int &argc, char **argv)
     : QApplication(argc, argv)
 {
     setApplicationName("scowser");
-    setApplicationVersion("0.0.4");
+    setApplicationVersion("0.0.5");
     setOrganizationName("scowser");
     setWindowIcon(QIcon(":/icons/scowser.png"));
 
